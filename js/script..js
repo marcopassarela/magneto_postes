4f6fbc0c25d0844d5e5e01f83b3ef83e9bbad951
marcopassarela <marcopassarela@hotmail.com>
2025-10-27 21:46:22 -0300

    glass theme

--- js/script..js
+++ js/script..js
@@ -9,3 +9,23 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+const menu = document.querySelector(".menu");
+const hoverBg = document.querySelector(".hover-bg");
+const items = document.querySelectorAll(".menu li");
+
+items.forEach(item => {
+  item.addEventListener("mouseenter", () => {
+    const rect = item.getBoundingClientRect();
+    const parentRect = menu.getBoundingClientRect();
+
+    hoverBg.style.width = `${rect.width}px`;
+    hoverBg.style.transform = `translateX(${rect.left - parentRect.left}px)`;
+    hoverBg.style.opacity = 1; // mostra a barra
+  });
+});
+
+// quando o mouse sair do menu, só muda a opacidade
+menu.addEventListener("mouseleave", () => {
+  hoverBg.style.opacity = 0;
+});
